f7bb18072d07d722a57d49114cb756288db799e5
Stanislav <skraev@smart.space>
2018-09-26 13:29:07 +0300

    Fix tests

--- tests/test_invalid_data.py
+++ tests/test_invalid_data.py
@@ -32,11 +32,11 @@ class TestInvalidData(TestCase):
             parse({"a": "20x", "b": 20}, Foo)
             self.assertTrue(False, "ValueError exception expected")
         except ValueError as exc:
-            self.assertEqual(("Unknown type `<class 'int'>` or invalid data: '20x'", 'a'), exc.args)
+            self.assertEqual(("invalid literal for int() with base 10: '20x'", 'a'), exc.args)
 
     def test_should_provide_failed_key_hierarchy_when_invalid_nested_data_parsed(self):
         try:
             parse({"d": {"a": "20x", "b": 20}, "e": 1}, Bar)
             self.assertTrue(False, "ValueError exception expected")
         except ValueError as exc:
-            self.assertEqual(("Unknown type `<class 'int'>` or invalid data: '20x'", 'a', 'd'), exc.args)
+            self.assertEqual(("invalid literal for int() with base 10: '20x'", 'a', 'd'), exc.args)
